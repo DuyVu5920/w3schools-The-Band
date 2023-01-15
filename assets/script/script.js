968803f917825ccdcf7cfed3dad877167f53f1a9
Vũ Trọng Duy <vduy5920@gmail.com>
2023-01-15 17:33:39 +0700

    3cm

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+var slideIndex = 0;
+autoSlider();
+
+function autoSlider() {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {
+		slideIndex = 1;
+	}
+	slides[slideIndex - 1].style.display = "block";
+	setTimeout(autoSlider, 3000);
+}
