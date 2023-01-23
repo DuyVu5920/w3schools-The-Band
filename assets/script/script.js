0885b6760dd4f1e8d5ee1b6840f4990ea3b06382
Vũ Trọng Duy <vduy5920@gmail.com>
2023-01-23 12:01:59 +0700

    add mobile menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,39 @@
 var slideIndex = 0;
-autoSlider();
+var mobileMenuToggleBtn = document.getElementById("toggle-menu-bar");
+var mobileMenu = document.getElementById("mobile-menu");
 var modal = document.getElementById("booking-modal");
+autoSlider();
 
 function autoSlider() {
 	var i;
-	var slides = document.getElementsByClassName("slides");
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
+	var slideList = document.getElementsByClassName("slides");
+	for (i = 0; i < slideList.length; i++) {
+		slideList[i].style.display = "none";
 	}
 	slideIndex++;
-	if (slideIndex > slides.length) {
+	if (slideIndex > slideList.length) {
 		slideIndex = 1;
 	}
-	slides[slideIndex - 1].style.display = "block";
+	slideList[slideIndex - 1].style.display = "block";
 	setTimeout(autoSlider, 3000);
+}
+
+function openMobileMenu() {
+	mobileMenu.style.display = "block";
+	mobileMenuToggleBtn.className = "bg-gray text-black";
+}
+
+function closeMobileMenu() {
+	mobileMenu.style.display = "none";
+	mobileMenuToggleBtn.className = "bg-black text-white";
+}
+
+function toggleMobileMenu() {
+	if (mobileMenu.style.display == "block") {
+		closeMobileMenu();
+	} else {
+		openMobileMenu();
+	}
 }
 
 function openModal() {
@@ -23,7 +43,7 @@ function openModal() {
 function closeModal() {
 	modal.style.display = "none";
 }
-
+//close modal when click outside
 window.onclick = function (event) {
 	if (event.target == modal) {
 		closeModal();
